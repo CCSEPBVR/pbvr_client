08eb2c99c75219032b60b912fb8980b2a96c4878
Martin Andersson <andersson.martin@jaea.go.jp>
2020-09-18 15:36:30 +0900

    Adding new DeferredTexture1D and ..2D classes to combat texture timing issues

---
+++ QtClient/QGlue/deferredtexture.h
@@ -0,0 +1,33 @@
+#ifndef DEFERREDTEXTURE_H
+#define DEFERREDTEXTURE_H
+
+#include <kvs/Texture>
+#include <kvs/Texture1D>
+#include <kvs/Texture2D>
+
+namespace  QGlue {
+
+class DeferredTexture2D: public kvs::Texture2D
+{
+
+public:
+    int __width;
+    int __height;
+    const void* __data;
+    bool complete=false;
+    void load( const size_t width, const size_t height, const void* data, const size_t xoffset=0, const size_t yoffset=0);
+    void bind();
+};
+
+class DeferredTexture1D: public kvs::Texture1D
+{
+
+public:
+    int __width;
+    const void* __data;
+    bool complete=false;
+    void load(int width, const void* data, bool hasContext);
+    void bind();
+};
+}
+#endif // DEFERREDTEXTURE_H
