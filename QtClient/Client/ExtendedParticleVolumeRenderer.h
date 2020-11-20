702904a7fac593a04d37daf92ad2762ded779fe7
Martin Andersson <andersson.martin@jaea.go.jp>
2020-11-20 15:31:07 +0900

    Merged ExtendedParticleVolumeRenderer from CS branch

--- QtClient/Client/ExtendedParticleVolumeRenderer.h
+++ QtClient/Client/ExtendedParticleVolumeRenderer.h
@@ -6,9 +6,9 @@
 #define RENDERER_BASE   kvs::ParticleVolumeRenderer
 #define BASE_INIT_ARGS  &point, subpixel_level
 #else
-#include "kvs/glew/ParticleVolumeRenderer"
-#define RENDERER_BASE   kvs::glew::ParticleVolumeRenderer
-#define BASE_INIT_ARGS  &(const_cast<kvs::PointObject&>(point)), subpixel_level, repeat_level
+#include <kvs/ParticleBasedRenderer>
+#define RENDERER_BASE   kvs::glsl::ParticleBasedRenderer
+#define BASE_INIT_ARGS  //&(const_cast<kvs::PointObject&>(point)), subpixel_level, repeat_level
 #endif
 
 namespace kvs
@@ -21,42 +21,22 @@ class ExtendedParticleVolumeRenderer : public RENDERER_BASE
 {
 public:
     ExtendedParticleVolumeRenderer( const kvs::PointObject& point, const size_t subpixel_level = 1, const size_t repeat_level = 1 ): RENDERER_BASE( BASE_INIT_ARGS )
-    {}
-
-    ~ExtendedParticleVolumeRenderer() {};
-
-
-    void changePointObject( const kvs::PointObject& point )
     {
-        this->attachPointObject( &point );
-#ifndef CPUMODE
-        this->align_particles();
-        this->create_vertexbuffer();
-#endif
+
     }
 
-#ifdef CPUMODE
     void recreateImageBuffer()
     {
-//        std::cout<<("THIS WASN'T CALLED FROM CPUMODE UNTIL INSITU MERGE")<<std::endl;
-//        std::cout<<"  ## recreateImageBuffer starts ## \n    size: "<<m_width<<":"<<m_height<<std::endl;
+#ifdef CPUMODE
         //KVS2.7.0
         //MOD BY)T.Osaki 2020.05.28
-        /*
-        this->delete_particle_buffer();
-        this->create_particle_buffer(
-                    BaseClass::m_width,
-                    BaseClass::m_height,
-                    m_subpixel_level );
-        */
         this->deleteParticleBuffer();
         this->createParticleBuffer(
                     BaseClass::windowWidth(),
                     BaseClass::windowHeight(),
                     m_subpixel_level );
-//        std::cout<<"  ## recreateImageBuffer ends: ##"<<std::endl;
-    }
 #endif
+    }
 
 };
 
