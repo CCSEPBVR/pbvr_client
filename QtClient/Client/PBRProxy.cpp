db725330764613ec7a122268cca373e669092f4d
Martin Andersson <andersson.martin@jaea.go.jp>
2021-02-10 15:50:24 +0900

    Adding new PBRProxy, and SwitchablePBRProxy classes which replaces the ExtendedParticleBasedRenderer class.  These new classes allow the runtime switching of GPU and CPU mode for the renderer.

---
+++ QtClient/Client/PBRProxy.cpp
@@ -0,0 +1,211 @@
+#include "PBRProxy.h"
+using namespace  kvs::visclient;
+
+
+/**
+ * @brief PBRProxy  - constructor
+ * @param mode_selection , Selects the mode of operation {CPU|GPU}
+ * @param other ,  other ojbect to replicate state from.
+ */
+PBRProxy::PBRProxy(   PBR_MODE mode_selection, PBRProxy* other):
+    selected_mode(mode_selection)
+{
+    if(other!=NULL){
+        this->setSubpixelLevel(other->subpixelLevel());
+        this->setShadingString(other->pbvr_shader_params.c_str());
+    }
+}
+
+/**
+ * @brief recreateImageBuffer, recreates the Image Buffer  for CPU renderer.
+ */
+void PBRProxy::recreateImageBuffer()
+{
+    if(selected_mode==GPU){
+        //KVS2.7.0
+        //MOD BY)T.Osaki 2020.05.28
+        this->deleteParticleBuffer();
+        this->createParticleBuffer(    CPU_BASE::windowWidth(), CPU_BASE::windowHeight(),  m_subpixel_level );
+    }
+}
+
+bool PBRProxy::isEnabledShading(){
+    return selected_mode==GPU?GPU_BASE::isEnabledShading():CPU_BASE::isEnabledShading();
+}
+/**
+ * @brief setRepetitionLevel, set renderer repetition level.
+ * @param rep_level
+ */
+void PBRProxy::setRepetitionLevel(size_t rep_level){
+    if (selected_mode==GPU)
+        GPU_BASE::setRepetitionLevel(rep_level);
+    else
+        CPU_BASE::setSubpixelLevel( (size_t) sqrt(rep_level));
+}
+/**
+ * @brief setSubpixelLevel, set subpixel level
+ * @param sp_level
+ */
+void PBRProxy::setSubpixelLevel(size_t sp_level){
+    if (selected_mode==GPU){
+        GPU_BASE::setRepetitionLevel(sp_level*sp_level);
+    }
+    else{
+        CPU_BASE::setSubpixelLevel( sp_level);
+    }
+}
+
+/**
+ * @brief setShader, set shader
+ * @param shader
+ */
+template <typename ShadingType>
+void PBRProxy::setShader( const ShadingType shader ){
+    selected_mode==GPU?GPU_BASE::setShader(shader):CPU_BASE::setShader(shader);
+}
+
+/**
+ * @brief disableShading, disable shading
+ */
+void PBRProxy::disableShading(){
+    selected_mode==GPU?GPU_BASE::disableShading():CPU_BASE::disableShading();
+}
+
+/**
+ * @brief enableShading, enable shading
+ */
+void PBRProxy::enableShading(){
+    selected_mode==GPU?GPU_BASE::enableShading():CPU_BASE::enableShading();
+}
+
+/**
+ * @brief repetitionLevel, return the repetitionLevel
+ * @return
+ */
+size_t PBRProxy::repetitionLevel()
+{
+    return (selected_mode==GPU) ? GPU_BASE::repetitionLevel():CPU_BASE::subpixelLevel() *  CPU_BASE::subpixelLevel();
+
+}
+/**
+ * @brief subpixelLevel, return the sub pixel level
+ * @return
+ */
+size_t PBRProxy::subpixelLevel()
+{
+    return selected_mode==GPU?(size_t) sqrt(GPU_BASE::repetitionLevel()):CPU_BASE::subpixelLevel();
+}
+/**
+ * @brief setShadingString , sets PBVR type shading string in format L|P|B[Ka,Kd][,Ks,S]
+ * @param pbvr_shading_string , L for Lambert, P for Phong, B for BlinnPhong
+ *                              Ka for ambient, Kd for diffuse,  (Available for L|P|B)
+ *                              Ks For Specular, S for Shinyness (Available for P|B)
+ */
+void PBRProxy::setShadingString(  const char* pbvr_shader_string)
+{
+    const char shaderType=pbvr_shader_string[0];
+    kvs::Shader::Base* b = shader_map[shaderType];
+    if (b){
+        // sscanf will only scan as many strings as matches the format, i.e if only two are available, then only two are scanned.
+        // All shaders have all 4 members Ka,Kd,Ks,S, but  Lambert will only use Ka,Kd.
+        int nscan=sscanf( &pbvr_shader_string[1],",%f,%f,%f,%f", &b->Ka, &b->Kd, &b->Ks, &b->S );
+        if(shaderType=='L') setShader(L);
+        if(shaderType=='B') setShader(B);
+        if(shaderType=='P') setShader(P);
+        enableShading();
+        this->pbvr_shader_params=pbvr_shader_string;
+    }
+    else{
+        disableShading();
+    }
+}
+
+
+SwitchablePBRProxy::SwitchablePBRProxy(bool use_gpu){
+    this->_use_gpu=use_gpu;
+    kvs::visclient::PBR_MODE m = _use_gpu?kvs::visclient::GPU:kvs::visclient::CPU;
+    pbr=new kvs::visclient::PBRProxy(m );
+}
+
+/**
+ * @brief use_gpu, switch to GPU mode - repcreates renderer [ need replacement in scene]
+ */
+void SwitchablePBRProxy::use_gpu(){
+    _use_gpu=true;
+    recreatePBR();
+}
+/**
+ * @brief use_cpu, switch to CPU mode - repcreates renderer [ need replacement in scene]
+ */
+void SwitchablePBRProxy::use_cpu(){
+    _use_gpu=false;
+    recreatePBR();
+}
+
+/**
+ * @brief enableShading enables shading
+ */
+void SwitchablePBRProxy::enableShading(){
+    pbr->enableShading();
+}
+
+/**
+ * @brief disableShading disables shading
+ */
+void SwitchablePBRProxy::disableShading(){
+    pbr->disableShading();
+}
+
+/**
+ * @brief setShadingString , sets PBVR type shading string in format L|P|B[Ka,Kd][,Ks,S]
+ * @param pbvr_shading_string , L for Lambert, P for Phong, B for BlinnPhong
+ *                              Ka for ambient, Kd for diffuse,  (Available for L|P|B)
+ *                              Ks For Specular, S for Shinyness (Available for P|B)
+ */
+void SwitchablePBRProxy::setShadingString(  const char* pbvr_shading_string)
+{
+    pbr->setShadingString(pbvr_shading_string);
+
+}
+/**
+ * @brief recreatePBR, recreates the PBR instance
+ */
+void SwitchablePBRProxy::recreatePBR(){
+    kvs::visclient::PBR_MODE m = _use_gpu?kvs::visclient::GPU:kvs::visclient::CPU;
+    auto new_pbr=new kvs::visclient::PBRProxy(m,pbr );
+    if (pbr){
+        delete pbr;
+        pbr=new_pbr;
+    }
+}
+/**
+ * @brief getRepetitionLevel , get the repetition level from PBR
+ * @return
+ */
+size_t SwitchablePBRProxy::getRepetitionLevel()
+{
+    assert (pbr->repetitionLevel() == pbr->subpixelLevel()* pbr->subpixelLevel());
+    return pbr->repetitionLevel();
+}
+/**
+ * @brief setRepetitionLevel, set the repetition level of PBR
+ * @param rep_level
+ */
+void SwitchablePBRProxy::setRepetitionLevel( const size_t rep_level){
+    recreatePBR();
+    pbr->setRepetitionLevel(rep_level);
+    assert (pbr->repetitionLevel() == pbr->subpixelLevel()* pbr->subpixelLevel());
+}
+
+/**
+ * @brief pbr_pointer, get the PBR pointer cast to kvs::RenderBase
+ * @return
+ */
+kvs::RendererBase* SwitchablePBRProxy::pbr_pointer(){
+    assert (pbr->repetitionLevel() == pbr->subpixelLevel()* pbr->subpixelLevel());
+
+    if (_use_gpu)
+        return   (kvs::glsl::ParticleBasedRenderer*)pbr;
+    else
+        return (kvs::ParticleBasedRenderer*)pbr;
+}
--- QtClient/Client/PBRProxy.h
+++ QtClient/Client/PBRProxy.h
@@ -1,186 +1,74 @@
-#ifndef PBVR__KVS__VISCLIENT__EXTENDED_PARTICLE_VOLUME_RENDERER_H_INCLUDE
-#define PBVR__KVS__VISCLIENT__EXTENDED_PARTICLE_VOLUME_RENDERER_H_INCLUDE
+#ifndef PBVR_PBR_PROXY_H_INCLUDE
+#define PBVR_PBR_PROXY_H_INCLUDE
 
-#ifdef CPUMODE
-#include <kvs/ParticleVolumeRenderer>
-#define RENDERER_BASE   kvs::ParticleVolumeRenderer
-#define BASE_INIT_ARGS  &point, subpixel_level // Init arguments for kvs::ParticleVolumeRenderer
-#else
 #include <kvs/ParticleBasedRenderer>
-#define RENDERER_BASE   kvs::glsl::ParticleBasedRenderer
-#define BASE_INIT_ARGS  // No init arguments for  base class kvs::glsl::ParticleBasedRenderer
-#endif
+#include <map>
 
 namespace kvs
 {
 namespace visclient
 {
 
+typedef  kvs::glsl::ParticleBasedRenderer GPU_BASE;
+typedef  kvs::ParticleBasedRenderer CPU_BASE;
+enum PBR_MODE { CPU,GPU};
 
-class ExtendedParticleVolumeRenderer : public kvs::ParticleBasedRenderer
+/**
+ * @brief The PBRProxy class, this acts a a proxy (in lack of a better term) class for the
+ *          kvs::ParticleBasedRenderer, and kvs::glsl::ParticleBasedRenderer classes.
+ *
+ *        Constructor takes a PBR_MODE to decide the mode of operation, and any subsequent calls
+ *        to underlying classes will be performed on the base class that corrsponds to the
+ *        mode of operation.
+ */
+class PBRProxy : public CPU_BASE, public GPU_BASE
 {
 public:
-    ExtendedParticleVolumeRenderer( const kvs::PointObject& point, const size_t subpixel_level = 1): kvs::ParticleBasedRenderer( &point, subpixel_level )
-    {
-
-    }
-    ExtendedParticleVolumeRenderer(  )
-    {
-
-    }
-    void recreateImageBuffer()
-    {
-        //KVS2.7.0
-        //MOD BY)T.Osaki 2020.05.28
-        //this->setDevicePixelRatio(2);
-        this->deleteParticleBuffer();
-        this->createParticleBuffer(
-                    BaseClass::windowWidth(),
-                    BaseClass::windowHeight(),
-                    m_subpixel_level );
-    }
-
-};
-
-}
-}
-
-
-class SwitchablePBR{
-    kvs::glsl::ParticleBasedRenderer* gpu_pbr;
-    kvs::visclient::ExtendedParticleVolumeRenderer* cpu_pbr;
-    char shaderType='U';
+    PBR_MODE selected_mode = GPU;
     kvs::Shader::Lambert L;
     kvs::Shader::BlinnPhong B;
     kvs::Shader::Phong P;
+    std::map<char, kvs::Shader::Base*> shader_map ={ {'L',&L}, {'B',&B},  {'P',&P}};
+    std::string pbvr_shader_params;
+    PBRProxy(   PBR_MODE mode_selection, PBRProxy* other=NULL);
+    void recreateImageBuffer();
+    bool isEnabledShading();
+    void setRepetitionLevel(size_t rep_level);
+    void setSubpixelLevel(size_t sp_level);
+    template <typename ShadingType>
+    void setShader( const ShadingType shader );
+    void disableShading();
+    void enableShading();
+    size_t repetitionLevel();
+    size_t subpixelLevel();
+    void setShadingString(  const char* pbvr_shader_string);
+};
 
+}
+}
+
+/**
+ * @brief  The SwitchablePBRProxy class, this class provides an interface to be able to switch between the two states of
+ *        the PBRProxy class.
+ *         Additionally, it automatically recreates the PBRProxy object when the RenderRepetitionLevel,
+ *        or GPU|CPU mode changes.
+ */
+class SwitchablePBRProxy{
+private:
+    kvs::visclient::PBRProxy* pbr; // ParticleBasedRenderer (proxy)
+    bool shading_enabled;
     bool _use_gpu=true;
 public:
-    SwitchablePBR(bool use_gpu=true){
-        this->_use_gpu=use_gpu;
-        cpu_pbr=new kvs::visclient::ExtendedParticleVolumeRenderer();
-        gpu_pbr=new kvs::glsl::ParticleBasedRenderer();
-    }
 
-    bool uses_gpu(){return _use_gpu;}
-    void enable_gpu(){_use_gpu=true;}
-    void disable_gpu(){_use_gpu=false;}
-
-
-
-    size_t repetitionLevel(){
-        assert (gpu_pbr->repetitionLevel() == cpu_pbr->subpixelLevel()* cpu_pbr->subpixelLevel());
-        return  gpu_pbr->repetitionLevel();
-    }
-
-
-    size_t subpixelLevel(){
-        assert (gpu_pbr->repetitionLevel() == cpu_pbr->subpixelLevel()* cpu_pbr->subpixelLevel());
-        return  cpu_pbr->subpixelLevel();
-    }
-
-    kvs::Shader::Base* getShaderBasePointer(){
-        if (shaderType=='P' )
-            return &this->P;
-        if (shaderType=='L' )
-            return &this->L;
-        if (shaderType=='B' )
-            return &this->B;
-        // Unknow shader type default to Lambert;
-        return NULL;
-
-    }
-
-    void enableShading(){
-        gpu_pbr->enableShading();
-        cpu_pbr->enableShading();
-    }
-
-    void disableShading(){
-        gpu_pbr->disableShading();
-        cpu_pbr->disableShading();
-    }
-
-    void setRendererShaders(){
-        gpu_pbr->enableShading();
-        cpu_pbr->enableShading();
-
-        if (shaderType=='P' ){
-            gpu_pbr->setShader(this->P);
-            cpu_pbr->setShader(this->P);
-        }
-        else if (shaderType=='L' ){
-            gpu_pbr->setShader(this->L);
-            cpu_pbr->setShader(this->L);
-        }
-        else if (shaderType=='B' ){
-            gpu_pbr->setShader(this->B);
-            cpu_pbr->setShader(this->B);
-        }
-        else{ // Unknown shader
-            gpu_pbr->disableShading();
-            cpu_pbr->disableShading();
-        }
-    }
-    //    template <typename ShadingType>
-    void setShaderString(  const char* shadingLevel)
-    {
-        this->shaderType=shadingLevel[0];
-        kvs::Shader::Base* b = getShaderBasePointer();
-        if (b)
-            int nscan=sscanf( &shadingLevel[1],",%f,%f,%f,%f", &b->Ka, &b->Kd, &b->Ks, &b->S );
-        else
-            shaderType='U';  //Unknown Shader
-        setRendererShaders();
-
-    }
-
-    void recreateGPUPBR(){
-        if (gpu_pbr)
-            delete gpu_pbr;
-        gpu_pbr=new kvs::glsl::ParticleBasedRenderer();
-        setRendererShaders();
-    }
-
-    void recreateCPUPBR(){
-        if (cpu_pbr)
-            delete cpu_pbr;
-        cpu_pbr=new kvs::visclient::ExtendedParticleVolumeRenderer();
-        setRendererShaders();
-
-    }
-
-    void setRepetitionLevel( const size_t rep_level){
-
-        size_t sp_level=ceil(sqrt(rep_level));
-        recreateGPUPBR();
-        gpu_pbr->setRepetitionLevel(rep_level);
-        recreateCPUPBR();
-        cpu_pbr->setSubpixelLevel(sp_level);
-
-        //        cpu_pbr->recreateImageBuffer();
-
-        assert (gpu_pbr->repetitionLevel() == cpu_pbr->subpixelLevel()* cpu_pbr->subpixelLevel());
-    }
-
-    void setSubpixelLevel( const size_t sp_level){
-
-        size_t rep_level= sp_level * sp_level;
-        recreateGPUPBR();
-        gpu_pbr->setRepetitionLevel(rep_level);
-        recreateCPUPBR();
-        cpu_pbr->setSubpixelLevel(sp_level);
-        //        cpu_pbr->recreateImageBuffer();
-
-        assert (gpu_pbr->repetitionLevel() == cpu_pbr->subpixelLevel()* cpu_pbr->subpixelLevel());
-    }
-
-    kvs::RendererBase* selected(){
-        assert (gpu_pbr->repetitionLevel() == cpu_pbr->subpixelLevel()* cpu_pbr->subpixelLevel());
-        if (_use_gpu)
-            return  gpu_pbr;
-        else
-            return cpu_pbr;
-    }
+    SwitchablePBRProxy(bool use_gpu=true);
+    void use_gpu();
+    void use_cpu();
+    void enableShading();
+    void disableShading();
+    void setShadingString(  const char* pbvr_shading_string);
+    void recreatePBR();
+    size_t getRepetitionLevel();
+    void setRepetitionLevel( const size_t rep_level);
+    kvs::RendererBase* pbr_pointer();
 };
-#endif    // PBVR__KVS__VISCLIENT__EXTENDED_PARTICLE_VOLUME_RENDERER_H_INCLUDE
+#endif    // PBVR_PBR_PROXY_H_INCLUDE
